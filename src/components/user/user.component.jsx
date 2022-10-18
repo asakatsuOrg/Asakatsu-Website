64f3c4b85e841408bc7d877cbf19b6c74b458565
Ali Reza <webdeve1083@gmail.com>
2022-10-18 16:11:39 +0800

    Deleted the skeleton style

--- src/components/user/user.component.jsx
+++ src/components/user/user.component.jsx
@@ -1,14 +1,9 @@
 import './user.style.scss'
 
-import { Skeleton } from '@mui/material'
-
 const User = ({ info }) => {
     return (
         <div className="user">
-            <div>
-                <Skeleton className='skeleton' variant="circular" width={50} height={50} />
-                <img src={ info.profileImg } alt="" />
-            </div>
+            <img src={ info.profileImg } alt="" />
             <div>
                 <p>{ info.name }</p>
                 <a href={ info.github } target="_blank">Check out { info.name }'s GitHub</a>
