78600a6e9ad17dc24a1b12ee11d936b8b9dd93a6
Ali Reza <webdeve1083@gmail.com>
2022-10-29 18:32:24 +0800

    for checking...

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -36,7 +36,7 @@ const Header = () => {
     return (
         <header>
             <div className='homeText'>
-                <h1>ASAKATSU</h1>
+                <h1>Asakatsu</h1>
                 <p>A project where you can keep track of your goal's progress, and contribute to open source in the same time.</p>
                 <p><a href="https://github.com/asakatsuOrg/AsaKatsuProject" target="_blank">AsaKatsu</a> is a Japanese term for waking up a little early to do something productive, in line with a goal. For example; read 4 books per month, do 100 days challenge, etc.</p>
                 <p>In this project, everyone who will commit will have to do a thing or two every morning and log the progress here.</p>  
