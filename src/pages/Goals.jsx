3227c0aca0e58bb3e5d2f3772635b53aa8c9733d
Ali Reza <webdeve1083@gmail.com>
2022-12-21 19:29:52 +0800

    added a text when there is no goals

--- src/pages/Goals.jsx
+++ src/pages/Goals.jsx
@@ -49,6 +49,11 @@ const Goals = () => {
           <EachGoals key={goal.id} goal={goal} deletingGoal={deletingGoal} />
         );
       })}
+      {goals.length == 0 && (
+        <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl md:text-5xl lg:text-8xl font-black animate-pulse">
+          No Goals
+        </h1>
+      )}
     </motion.div>
   );
 };
