8bd6f8e4b7faf2bf16a2136f345f7e0e9f8fb206
Ali Reza <webdeve1083@gmail.com>
2022-12-25 19:32:26 +0800

    edited the FAQ section

--- src/content/FAQ.js
+++ src/content/FAQ.js
@@ -2,19 +2,13 @@ export const FAQ = [
   {
     id: 1,
     question: "What is Asakatsu?",
-    answer:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse perferendis facilis, dolorum atque, aliquam earum eligendi quisquam excepturi minima veniam deserunt, repellat eaque doloremque perspiciatis! Exercitationem aspernatur eveniet possimus necessitatibus.",
+    answer: `A website where you can keep track of your goal's progress.
+      AsaKatsu is a Japanese term for waking up a little early to do something productive, in line with a goal. For example; read 4 books per month, do 100 days challenge, etc.`,
   },
   {
     id: 2,
     question: "What should we do?",
     answer:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse perferendis facilis, dolorum atque, aliquam earum eligendi quisquam excepturi minima veniam deserunt, repellat eaque doloremque perspiciatis! Exercitationem aspernatur eveniet possimus necessitatibus.",
-  },
-  {
-    id: 3,
-    question: "What is Asakatsu?",
-    answer:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse perferendis facilis, dolorum atque, aliquam earum eligendi quisquam excepturi minima veniam deserunt, repellat eaque doloremque perspiciatis! Exercitationem aspernatur eveniet possimus necessitatibus.",
+      "There are lots of Goals for a person to achieve them, so It is better to write them somewhere and achieve them one by one.",
   },
 ];
