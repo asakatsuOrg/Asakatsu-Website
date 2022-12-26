6619133738866331d8baebc5fb3802ce6040c5d4
Ali Reza <webdeve1083@gmail.com>
2022-12-27 06:33:36 +0800

    Fixed a bug in styling the save component

--- src/components/IsSaved.jsx
+++ src/components/IsSaved.jsx
@@ -6,7 +6,7 @@ const IsSaved = () => {
     <motion.div
       animate={{ opacity: [0, 1], x: [100, -20, 0] }}
       transition={{ duration: 0.3 }}
-      className={`fixed left-4 bottom-4 md:right-4 md:top-0 flex items-center gap-2`}>
+      className={`fixed right-0 flex items-center gap-2 z-50 bg-background bg-opacity-50 dark:bg-white dark:bg-opacity-50 backdrop-blur-sm py-2 px-4 rounded-l-full`}>
       Saved!
       <img
         className="w-[20px]"
