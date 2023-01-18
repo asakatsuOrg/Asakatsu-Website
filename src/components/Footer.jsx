37d0c5e886f1cecea43acaa20f8615761cce0125
Ali Reza <webdeve1083@gmail.com>
2023-01-18 09:16:45 +0800

    Fixed some styling issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full h-16 bg-[#1C1C1C] absolute bottom-0 left-0">
-      Footer
+    <div className="w-full h-16 bg-[#1C1C1C] absolute bottom-0 left-0 flex justify-center items-end text-white text-opacity-60 py-2">
+      Asakatsu | Developed by Asakatsu Team &#169; {new Date().getFullYear()}
     </div>
   );
 };
--- src/components/Home/GetStarted.jsx
+++ src/components/Home/GetStarted.jsx
@@ -5,14 +5,14 @@ import { ReactComponent as GetStartedSVG } from "../../assets/get started.svg";
 
 const GetStarted = () => {
   return (
-    <div className="w-full max-w-[1536px] pt-24 px-4 lg:px-20 xl:px-56 flex flex-col items-center">
+    <div className="w-full max-w-[1536px] py-24 md:py-0 md:pt-24 px-4 lg:px-20 xl:px-56 flex flex-col items-center">
       <h1 className="text-center text-3xl md:text-[6vw] lg:text-5xl font-bold mb-12">
         What are you waiting for?
       </h1>
       <Button path={"/authentication"} buttonType={"bigger"}>
         Get Started
       </Button>
-      <GetStartedSVG className="h-96" />
+      <GetStartedSVG className="h-96 hidden md:block" />
     </div>
   );
 };
